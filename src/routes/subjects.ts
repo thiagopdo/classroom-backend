c522790898789a151dc93574ca4066caf1082cae
Thiago Oliveira <88551879+thiagopdo@users.noreply.github.com>
2026-01-21 23:08:03 -0300

    feat: added GET all subjects route

---
+++ src/routes/subjects.ts
@@ -0,0 +1,72 @@
+import { and, desc, eq, getTableColumns, ilike, or, sql } from "drizzle-orm";
+import express from "express";
+import { departments, subjects } from "../db/schema";
+import { db } from "../db";
+
+const router = express.Router();
+
+//Get all subjects with optional department filter and pagination
+router.get("/", async (req, res) => {
+  try {
+    const { search, department, page = 1, limit = 10 } = req.query;
+
+    const currentPage = Math.max(1, +page);
+    const limitPerPage = Math.max(1, +limit);
+
+    const offset = (currentPage - 1) * limitPerPage;
+
+    const filterConditions = [];
+
+    //if search query exists, filter by name OR subject code
+    if (search) {
+      filterConditions.push(
+        or(
+          ilike(subjects.name, `%${search}%`),
+          ilike(subjects.code, `%${search}%`),
+        ),
+      );
+    }
+    //if department query exists, filter by departmentId
+    if (department) {
+      filterConditions.push(ilike(departments.name, `%${department}%`));
+    }
+
+    //Combine all filters using AND if any filters exist
+    const whereClause =
+      filterConditions.length > 0 ? and(...filterConditions) : undefined;
+
+    const countResult = await db
+      .select({ count: sql<number>`count(*)` })
+      .from(subjects)
+      .leftJoin(departments, eq(subjects.departmentId, departments.id))
+      .where(whereClause);
+    const totalCount = countResult[0]?.count ?? 0;
+
+    const subjectsList = await db
+      .select({
+        ...getTableColumns(subjects),
+        department: { ...getTableColumns(departments) },
+      })
+      .from(subjects)
+      .leftJoin(departments, eq(subjects.departmentId, departments.id))
+      .where(whereClause)
+      .orderBy(desc(subjects.createdAt))
+      .limit(limitPerPage)
+      .offset(offset);
+
+    res.status(200).json({
+      data: subjectsList,
+      pagination: {
+        page: currentPage,
+        limit: limitPerPage,
+        total: totalCount,
+        totalPages: Math.ceil(totalCount / limitPerPage),
+      },
+    });
+  } catch (error) {
+    console.log(`Get /subjects error: ${error}`);
+    res.status(500).json({ error: "Failed to get the subjects" });
+  }
+});
+
+export default router;
